9abc12b38e970cf976cfda7e065e6fbfc66bf791
talbi21 <achref.talbi@esprit.tn>
2023-04-03 05:45:42 +0100

    Create task.js

---
+++ routes/task.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+let User = require('../models/task');
+
+router.get('/tasks', (req, res) => {
+    Task.find()
+      .then(tasks => res.json(tasks))
+      .catch(err => res.status(500).json({ error: err }));
+  });
+  
+  router.post('/tasks', (req, res) => {
+    const { title, date, description, status, type } = req.body;
+    const newTask = new Task({ title, date, description, status, type });
+    newTask.save()
+      .then(task => res.json(task))
+      .catch(err => res.status(500).json({ error: err }));
+  });
+  
+  router.get('/tasks/:id', (req, res) => {
+    Task.findById(req.params.id)
+      .then(task => res.json(task))
+      .catch(err => res.status(500).json({ error: err }));
+  });
+  
+  router.put('/tasks/:id', (req, res) => {
+    Task.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      .then(task => res.json(task))
+      .catch(err => res.status(500).json({ error: err }));
+  });
+  
+  router.delete('/tasks/:id', (req, res) => {
+    Task.findByIdAndDelete(req.params.id)
+      .then(() => res.json({ success: true }))
+      .catch(err => res.status(500).json({ error: err }));
+  });
+
+  module.exports = router;
